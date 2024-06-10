02992b849d2f2953308c48da5769fb866e0f7164
Hemanth Kumar <hemanthhm693@gmail.com>
2024-06-10 22:07:24 +0530

    first file added

---
+++ simplejsode.js
@@ -0,0 +1,21 @@
+let age=14
+
+if(age>=18)
+    console.log("A person can vote")
+else 
+    console.log("A person can not vote")
+
+console.log("Program execution is done babe")
+
+/*
+        output:
+
+            A person can not vote
+            Program execution is done babe
+
+        How to excute the node program
+
+            In the first note i learn how to execute the node and how to use it to run the node js we need to use 
+            
+            node app.js or node app (in general) node (file_name).js or node (file_name)
+*/
